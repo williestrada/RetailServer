980930d57ca8b11cc016c3713cdb26ce4d40058a
Willie <williestrada@yahoo.com>
2020-06-15 12:34:33 +0800

    Data Connection

--- server.js
+++ server.js
@@ -35,4 +35,8 @@ mongoose.connect(
   }
 );
 
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected to Database"));
+
 //app.listen(3000);
